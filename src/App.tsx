9f89a05849182434de7957688c329fead36de92b
Kazuho1222 <destiny5914@gmail.com>
2024-07-14 20:17:37 +0900

    css修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { FC, useState } from 'react';
 import './App.css';
 
@@ -16,32 +17,48 @@ export const App: FC<Props> = ({ onChange }) => {
   const [selectedTabText, setSelectedTabText] = useState<string>(contents[0].text)
 
   return (
-    <>
-      <div className='flex container mx-auto w-1/2'>
-        <div className='flex container items-start h-1/5 bg-white rounded-md rounded-b-none'>
-          <div className='flex container justify-between h-12 border-blue-600 border-solid place-items-center font-bold cursor-pointer bg-slate-300 rounded-md rounded-b-none mx-auto text-black items-center p-4'>
-            {...contents.map((content, index) => (
-              <>
-                <div
-                  onClick={() => {
-                    setTab(index);
-                    setSelectedTabText(content.text);
-                    onChange(tab);
-                  }}
-                >
-                  {content.text}
-                </div>
-              </>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className='flex justify-center container w-1/2 font-bold mx-auto items-center h-80 text-black rounded-md rounded-t-none bg-white text-center text-7xl'>
-        {selectedTabText}
-      </div>
-
-
-    </>
+    <div className='container mx-auto bg-white rounded-md shadow-md h-96 flex-col p-2'>
+      <nav>
+        <ul className='flex container justify-between h-12 border-slate-200 font-bold cursor-pointer rounded-md rounded-b-none mx-auto text-black w-full transition-colors text-center border-b-2 bg-white'>
+          {contents.map((content, index) => (
+            <li
+              className={`flex items-center justify-center rounded-md rounded-b-none w-full h-full relative ${index === tab ? 'bg-slate-200' : ''}`}
+              key={content.path}
+              onClick={() => {
+                setTab(index);
+                setSelectedTabText(content.text);
+                onChange(index)
+              }}
+            >
+              <span className='flex items-center justify-center w-full h-full'>{content.text}</span>
+              {index === tab && (
+                <motion.div
+                  className='underline absolute bottom-0 '
+                  layoutId="underline"
+                  style={{ borderBottom: "2px solid blue" }}
+                  initial={{ width: 0 }}
+                  animate={{ width: "100%" }}
+                  transition={{ duration: 0.5 }}
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main className='font-bold text-7xl text-black h-full flex justify-center items-center'>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={tab}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {selectedTabText}
+          </motion.div>
+        </AnimatePresence>
+      </main>
+    </div >
   )
 }
 export default App;
